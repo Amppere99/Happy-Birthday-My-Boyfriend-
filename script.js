5270068ebda22684e359e3a8efe6c7927c241ce7
Amppere <101197241+Amppere99@users.noreply.github.com>
2026-07-09 01:14:04 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const button = document.getElementById("openBook");
+
+button.onclick = () => {
+
+button.innerHTML = "Opening... ❤️";
+
+setTimeout(()=>{
+
+alert("The Memory Book will open in the next part 💙");
+
+},900);
+
+}
