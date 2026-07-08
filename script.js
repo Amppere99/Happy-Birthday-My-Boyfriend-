12786dfe943566d1b68fc0c93c84f7d067d3e246
Amppere <101197241+Amppere99@users.noreply.github.com>
2026-07-09 01:17:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ alert("The Memory Book will open in the next part 💙");
 },900);
 
 }
+const cover = document.getElementById("cover");
+const open = document.getElementById("openBook");
+
+open.onclick = () => {
+
+    cover.style.transform =
+        "rotateY(-165deg)";
+
+    cover.style.transition =
+        "1.4s";
+
+}
